aa678faebadca623a51a6128429e1323cb40064d
Ubuntu <ubuntu@ip-172-31-37-137.ap-northeast-1.compute.internal>
2020-04-13 07:00:32 +0000

    stripe peyment

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'dotenv-rails'
 gem 'faker', '=1.9.3'
 gem 'kaminari'
 
+gem 'stripe', '=4.18.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_090436) do
+ActiveRecord::Schema.define(version: 2020_04_13_065535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -154,6 +154,9 @@ ActiveRecord::Schema.define(version: 2020_04_02_090436) do
     t.string "provider"
     t.string "uid"
     t.string "image"
+    t.string "stripe_last_4"
+    t.string "stripe_id"
+    t.string "paypal"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
