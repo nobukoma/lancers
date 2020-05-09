fee81c9d7c93c6f301e4b0143d6641b91df1da85
Ubuntu <ubuntu@ip-172-31-37-137.ap-northeast-1.compute.internal>
2020-05-09 13:45:08 +0000

    Real-time Message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,21 +3,20 @@ class MessagesController < ApplicationController
     if current_user.id == message_params[:receiver_id]
       redirect_to request.referrer, alert: "You cannot send message to yourself"
     end
-    
+
     conversation = Conversation.where("(sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)",
                                       current_user.id, message_params[:receiver_id], 
                                       message_params[:receiver_id], current_user.id
                                     ).first
-    
     if !conversation.present?
       conversation = Conversation.create(sender_id: current_user.id, receiver_id: message_params[:receiver_id])
     end
-    
+
     @message = Message.new(user_id: current_user.id, 
                            conversation_id: conversation.id,
                            content: message_params[:content]
-                          )
-    
+    )
+
     if @message.save
       conversation.update!(updated_at: @message.created_at)
       receiver = conversation.sender.id == current_user.id ? conversation.receiver : conversation.sender
@@ -30,13 +29,13 @@ class MessagesController < ApplicationController
       if URI(request.referrer).path == conversation_detail_path(id: receiver.id)
         return render json: {success: true}
       end
+
       redirect_to request.referrer, notice: "Message sent..."
     else
       redirect_to request.referrer, alert: "Cannot send the message"
     end
-    
   end
-  
+
   private
 
   def render_message(message, user)
