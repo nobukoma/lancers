e077efa16d90d7ac8d03f4e12d543f1656f99a9f
Ubuntu <ubuntu@ip-172-31-37-137.ap-northeast-1.compute.internal>
2020-05-10 14:29:35 +0000

    Create Comments on Order Page

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,8 @@ Rails.application.routes.draw do
   post '/settings/payment', to: 'users#update_payment', as: 'update_payment'
   post '/settings/payout', to: 'users#update_payout', as: 'update_payout'
   post '/users/withdraw', to: 'users#withdraw', as: 'withdraw'
-  post 'messages', to: 'messages#create' 
+  post 'messages', to: 'messages#create'
+  post '/comments', to: 'comments#create'
   
   put 'orders/:id/complete', to: 'orders#complete', as:'complete_order'
   put '/offers/:id/accept', to: 'offers#accept', as: 'accept_offer'
