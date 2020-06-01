a5148dad2ec43545341e8b8ccee932931b5b9981
Ubuntu <ubuntu@ip-172-31-37-137.ap-northeast-1.compute.internal>
2020-06-01 04:22:44 +0000

    to-heroku

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Lancers
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.hosts << ".amazonaws.com"
+    config.hosts << "torimotest01.herokuapp.com"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
